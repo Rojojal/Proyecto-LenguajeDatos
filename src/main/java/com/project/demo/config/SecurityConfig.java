3b015f623c4851ad3e441b6c44b7650c7d3f19c4
Garrett-Jackson11 <jacgogarrett@gmail.com>
2024-07-06 10:07:47 -0600

    Arreglos

--- src/main/java/com/project/demo/config/SecurityConfig.java
+++ src/main/java/com/project/demo/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.project.demo.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -25,18 +28,36 @@ public class SecurityConfig extends WebSecurityConfiguration {
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/register").permitAll()
                 .requestMatchers("/login").permitAll()
-                .requestMatchers("/logout").permitAll()
+                .requestMatchers("/usuarios/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                 .defaultSuccessUrl("/", true)
                 )
                 .logout(config -> config
-                .logoutSuccessUrl("/"))
+                .logoutSuccessUrl("/").permitAll())
+                
+                
                 .build();
+                
                 
 
 
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails client = org.springframework.security.core.userdetails.User.withUsername("clientUser")
+            .password(passwordEncoder().encode("clientPass"))
+            .roles("client")
+            .build();
+
+        UserDetails admin = org.springframework.security.core.userdetails.User.withUsername("adminUser")
+            .password(passwordEncoder().encode("adminPass"))
+            .roles("admin")
+            .build();
+
+        return new InMemoryUserDetailsManager(client, admin);
     }
 
 
@@ -44,5 +65,7 @@ public class SecurityConfig extends WebSecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+   
 
 }
--- src/main/java/com/project/demo/controllers/HomeController.java
+++ src/main/java/com/project/demo/controllers/HomeController.java
@@ -3,23 +3,16 @@ package com.project.demo.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.project.demo.models.register.Register;
 import com.project.demo.models.register.RegisterDto;
-
 import com.project.demo.services.RegisterRepository;
 
 import jakarta.validation.Valid;
@@ -33,15 +26,13 @@ public class HomeController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
     @GetMapping("/")
-    public String goHome(){
+    public String goHome() {
         return "index";
     }
 
     @GetMapping("/sigin")
-    public String login(){
-
+    public String login() {
         return "login";
     }
 
@@ -55,21 +46,21 @@ public class HomeController {
 
     @PostMapping("/register")
     public String register(@Valid @ModelAttribute("registerDto") RegisterDto registerDto,
-            BindingResult result, Model model) {
-        
+                           BindingResult result, Model model) {
+
         if (!registerDto.getPassword().equals(registerDto.getConfirmPassword())) {
-            model.addAttribute("error", "Password and Confirm Password  do not match");
-            return "register"; 
+            model.addAttribute("error", "Password and Confirm Password do not match");
+            return "register";
         }
 
         if (result.hasErrors()) {
-            return "register"; 
+            return "register";
         }
 
         Optional<Register> existingUser = repo.findByNombreUsuario(registerDto.getNombreUsuario());
         if (existingUser.isPresent()) {
             model.addAttribute("msg", "Username already exists");
-            return "register"; 
+            return "register";
         }
 
         Register register = new Register();
@@ -84,12 +75,11 @@ public class HomeController {
         Register savedUsuario = repo.save(register);
 
         if (savedUsuario != null) {
-            model.addAttribute("msg", "Register Sucessfully");
+            model.addAttribute("success", true);
         } else {
-            model.addAttribute("error", "Something went wrong  on the server");
+            model.addAttribute("error", "Something went wrong on the server");
         }
 
-        return "redirect:/login";
+        return "register";
     }
-
 }
